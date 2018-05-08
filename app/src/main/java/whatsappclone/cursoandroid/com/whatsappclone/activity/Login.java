16a97c1756a044c7c0e27a64f42cd4d73ee0e4f8
gabsnery <gneri94@gmail.com>
2018-05-08 12:06:20 -0300

    colocação mascaras em numeros

--- app/src/main/java/whatsappclone/cursoandroid/com/whatsappclone/activity/Login.java
+++ app/src/main/java/whatsappclone/cursoandroid/com/whatsappclone/activity/Login.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.constraint.solver.widgets.Snapshot;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,9 +21,29 @@ import whatsappclone.cursoandroid.com.whatsappclone.R;
 
 public class Login extends Activity {
 
+    private EditText editTextTelefone;
+    private EditText editTextDDD;
+    private EditText editTextCodigoPais;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+        editTextDDD = (EditText) findViewById(R.id.editTextDDD);
+        editTextCodigoPais = (EditText) findViewById(R.id.editTextCodPais);
+
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(editTextTelefone, simpleMaskTelefone);
+        editTextTelefone.addTextChangedListener(maskTelefone);
+
+        SimpleMaskFormatter simpleMaskDDD = new SimpleMaskFormatter("NN");
+        MaskTextWatcher maskDDD = new MaskTextWatcher(editTextDDD, simpleMaskDDD);
+        editTextDDD.addTextChangedListener(maskDDD);
+
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(editTextCodigoPais, simpleMaskCodPais);
+        editTextCodigoPais.addTextChangedListener(maskCodPais);
     }
 }
